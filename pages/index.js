6cbba308439ee72a74334180fe20b6e793db5518
ronan <row_none@yahoo.com>
2020-08-19 09:52:54 +0800

    first edit

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <p>Discover and deploy boilerplate example Next.js projects...</p>
           </a>
 
           <a
