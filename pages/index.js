d285529fcaccdb2b1ab34da7e815f9b84c422758
ronan <row_none@yahoo.com>
2020-08-19 10:01:38 +0800

    updates

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,15 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>HNS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+		<h1>Ronan</h1>
+        <h3 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </h3>
 
         <p className={styles.description}>
           Get started by editing{' '}
